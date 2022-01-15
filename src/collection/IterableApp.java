f6127e5eb8e4041a614d59cd9403bc4b78562dd1
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-01-15 09:42:34 +0700

    add Iterable & Iterator

---
+++ src/collection/IterableApp.java
@@ -0,0 +1,21 @@
+package collection;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class IterableApp {
+    public static void main(String[] args) {
+
+        Iterable<String> names = List.of("Rifqi", "Muhammad", "Aziz");
+        for (var name : names) {
+            System.out.println(name);
+        }
+
+        // Manual
+        Iterator<String> iterator = names.iterator();
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            System.out.println(name);
+        }
+    }
+}
