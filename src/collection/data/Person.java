1666a6b839178192c5ced67f3c0c02f2a617ce15
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-01-15 12:12:01 +0700

    add Mutable & Immutable List

---
+++ src/collection/data/Person.java
@@ -0,0 +1,30 @@
+package collection.data;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Person {
+
+    private String name;
+    private List<String> hobbies;
+
+    // Mutable (bisa berubah-ubah)
+    public Person(String name) {
+        this.name = name;
+        this.hobbies = new ArrayList<>();
+    }
+
+    public void addHobby(String hobby) {
+        hobbies.add(hobby);
+    }
+
+    public List<String> getHobbies() {
+        // return hobbies; MUTABLE
+        return Collections.unmodifiableList(hobbies); // IMMUTABLE
+    }
+
+    public String getName() {
+        return name;
+    }
+}
