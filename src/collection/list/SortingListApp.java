b54ba612b2172e637fe8736c54228db639ed66bf
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-01-17 12:53:46 +0700

    add Sorting List

--- src/collection/list/SortingListApp.java
+++ src/collection/list/SortingListApp.java
@@ -1,4 +1,38 @@
 package collection.list;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class SortingListApp {
+    public static void main(String[] args) {
+
+        System.out.println("===== Ascending Sorting =====");
+        // mutable
+        List<String> list = new ArrayList<>();
+        list.addAll(List.of("Rifqi", "Muhammad", "Aziz", "Xenosty", "Theord"));
+
+        // sorting list (ascending letter)
+        Collections.sort(list);
+        for (var value : list) {
+            System.out.println(value);
+        }
+
+        System.out.println("===== Descending Sorting using Comparator =====");
+
+        // create comparator to descending sorting
+        Comparator<String> reverse = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.compareTo(o1);
+            }
+        };
+
+        // sort using comparator
+        Collections.sort(list, reverse);
+        for (var value : list) {
+            System.out.println(value);
+        }
+    }
 }
